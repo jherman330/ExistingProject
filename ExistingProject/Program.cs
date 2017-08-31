2d4b467a4ed8a9e236970d031008f5afb7cf4077
john.herman <john.herman@MT_STOW_1544>
2017-08-31 10:33:53 -0400

    Add project files.

---
+++ ExistingProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExistingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //oops, havent added this to GIT yet so I better take a look at how to do that 
+            //before add to much code
+        }
+    }
+}
